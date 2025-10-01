457baa889ee0b6fdf92029e456abddc47d24bbe2
Rech Tan <rechtan@icloud.com>
2025-10-01 23:03:34 +0800

    add jake-stewart/multicursor.nvim plugin.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -43,4 +43,66 @@ return {
       { "<leader>uu", "<cmd>UndotreeToggle<CR>", desc = "Toggle undotree" },
     },
   },
+  {
+    "jake-stewart/multicursor.nvim",
+    branch = "1.0",
+    event = "VeryLazy",
+    config = function()
+      local mc = require("multicursor-nvim")
+      mc.setup()
+
+      local set = vim.keymap.set
+
+      set({ "n", "x" }, "<leader>mk", function()
+        mc.lineAddCursor(-1)
+      end, { desc = "Multi: add cursor above" })
+
+      set({ "n", "x" }, "<leader>mj", function()
+        mc.lineAddCursor(1)
+      end, { desc = "Multi: add cursor below" })
+
+      set({ "n", "x" }, "<leader>mn", function()
+        mc.matchAddCursor(1)
+      end, { desc = "Multi: add next match cursor" })
+
+      set({ "n", "x" }, "<leader>mN", function()
+        mc.matchAddCursor(-1)
+      end, { desc = "Multi: add previous match cursor" })
+
+      set({ "n", "x" }, "<leader>ms", function()
+        mc.matchSkipCursor(1)
+      end, { desc = "Multi: skip next match" })
+
+      set({ "n", "x" }, "<leader>mS", function()
+        mc.matchSkipCursor(-1)
+      end, { desc = "Multi: skip previous match" })
+
+      set({ "n", "x" }, "<leader>mA", mc.matchAllAddCursors, { desc = "Multi: add cursors to all matches" })
+
+      set({ "n", "x" }, "<leader>mm", mc.toggleCursor, { desc = "Multi: toggle cursor enable" })
+
+      set({ "n", "x" }, "<leader>mc", function()
+        if mc.hasCursors() then
+          mc.clearCursors()
+        end
+      end, { desc = "Multi: clear cursors" })
+
+      set("n", "<C-LeftMouse>", mc.handleMouse, { desc = "Multi: add cursor with mouse" })
+      set("n", "<C-LeftDrag>", mc.handleMouseDrag, { desc = "Multi: drag multi cursor selection" })
+      set("n", "<C-LeftRelease>", mc.handleMouseRelease, { desc = "Multi: finish multi cursor drag" })
+
+      mc.addKeymapLayer(function(layerSet)
+        layerSet({ "n", "x" }, "<leader>m,", mc.prevCursor)
+        layerSet({ "n", "x" }, "<leader>m.", mc.nextCursor)
+        layerSet({ "n", "x" }, "<leader>mx", mc.deleteCursor)
+        layerSet("n", "<esc>", function()
+          if not mc.cursorsEnabled() then
+            mc.enableCursors()
+          else
+            mc.clearCursors()
+          end
+        end)
+      end)
+    end,
+  },
 }
