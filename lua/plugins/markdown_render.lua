82e25e5f775929994509a2522cb048aae392abdf
Rech Tan <personal@rechtan.com>
2025-10-12 00:29:47 +0800

    add iamcco/markdown-preview.nvim plugin

--- lua/plugins/markdown_render.lua
+++ lua/plugins/markdown_render.lua
@@ -23,5 +23,21 @@ return {
       },
     },
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = { "markdown", "md", "markdown.mdx" },
+    build = "cd app && npm install",
+    init = function()
+      -- leave browser window open; manually stop with :MarkdownPreviewStop
+      vim.g.mkdp_auto_close = 0
+    end,
+    keys = {
+      {
+        "<leader>mp",
+        "<cmd>MarkdownPreviewToggle<CR>",
+        desc = "Markdown: Browser preview",
+        ft = { "markdown" },
+      },
+    },
+  },
 }
-
